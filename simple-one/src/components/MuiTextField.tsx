624bdb59b34ed66efd525a0a53091f319ff561bd
Anatoliy <devcodemy@gmail.com>
2023-02-26 19:42:08 +0200

    colors

--- simple-one/src/components/MuiTextField.tsx
+++ simple-one/src/components/MuiTextField.tsx
@@ -4,17 +4,23 @@ import '../styles/MuiTextField.css'
 
 export const MuiTextField = () => {
     return (
-        <Stack spacing={4}>
+        <Stack spacing={2}>
             <Stack direction='row' spacing={2}>
-                <TextField label='Input' variant='outlined'/>
-                <TextField label='Input' variant='filled'/>
-                <TextField label='Input' variant='standard'/>
+                <TextField label='Input' variant='filled' size='small'/>
+                <TextField label='Input' variant='standard' size='small'/>
+                <TextField label='Input' variant='outlined' size='small'/>
             </Stack>
             <Stack direction='row' spacing={2}>
                 <TextField label='Small secondary' size='small' color='secondary'/>
                 <TextField
                     label='Disabled prompt'
                     size='small'
+                    disabled
+                />
+                <TextField
+                    type='password'
+                    size='small'
+                    placeholder='some password'
                     disabled
                 />
             </Stack>
@@ -24,6 +30,51 @@ export const MuiTextField = () => {
                     label='Password'
                     helperText='Do not share you password with anyone!'
                     type='password'
+                />
+                <TextField
+                    label='Read only'
+                    placeholder='readonly value'
+                    InputProps={{ readOnly: true }}
+                />
+            </Stack>
+            <Stack direction='row' spacing={2}>
+                <TextField
+                    label='Info - by default'
+                    placeholder='color= info'
+                    size='small'
+                    color='info'
+                />
+                <TextField
+                    label='Primary - like Info'
+                    placeholder='color= primary'
+                    size='small'
+                    color='primary'
+                />
+                <TextField
+                    label='Secondary - magenta'
+                    placeholder='color= secondary'
+                    size='small'
+                    color='secondary'
+                />
+            </Stack>
+            <Stack direction='row' spacing={2}>
+                <TextField
+                    label='Success - green!'
+                    placeholder='color= success'
+                    size='small'
+                    color='success'
+                />
+                <TextField
+                    label='Warning - warm!'
+                    placeholder='color= warning'
+                    size='small'
+                    color='warning'
+                />
+                <TextField
+                    label='Error - red!'
+                    placeholder='color= error'
+                    size='small'
+                    color='error'
                 />
             </Stack>
         </Stack>
