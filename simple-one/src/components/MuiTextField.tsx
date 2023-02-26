6a3e33779e845f070d791f2bc94b8dbe23971b73
Anatolii <anatolii@mb-air.local>
2023-02-26 19:14:21 +0200

    styling text fields

--- simple-one/src/components/MuiTextField.tsx
+++ simple-one/src/components/MuiTextField.tsx
@@ -6,7 +6,25 @@ export const MuiTextField = () => {
     return (
         <Stack spacing={4}>
             <Stack direction='row' spacing={2}>
-                <TextField label='Input' />
+                <TextField label='Input' variant='outlined'/>
+                <TextField label='Input' variant='filled'/>
+                <TextField label='Input' variant='standard'/>
+            </Stack>
+            <Stack direction='row' spacing={2}>
+                <TextField label='Small secondary' size='small' color='secondary'/>
+                <TextField
+                    label='Disabled prompt'
+                    size='small'
+                    disabled
+                />
+            </Stack>
+            <Stack direction='row' spacing={2}>
+                <TextField label='Form input' required />
+                <TextField
+                    label='Password'
+                    helperText='Do not share you password with anyone!'
+                    type='password'
+                />
             </Stack>
         </Stack>
     )
